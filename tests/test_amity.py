d940b319f2373675ac6c119d0bfa92b4ec40a61a
andela-wcyn <cynthia.abura@andela.com>
2017-02-23 20:09:31 +0300

    [Chore] Add create room tests

--- tests/test_amity.py
+++ tests/test_amity.py
@@ -26,24 +26,44 @@ class TestAmity(unittest.TestCase):
         del self.living_space 
 
     def test_rooms_attr_is_list(self):
+        print("FAiling please..")
         self.assertIsInstance(self.amity.rooms, list)
 
     def test_rooms_attr_is_list_of_room_objs(self):
-        for i in self.amity.rooms:
-            self.assertIsInstance(i, Room)
+        if self.amity.rooms:
+            print("Something")
+            # To Avoid NoneType Error
+            for i in self.amity.rooms:
+                self.assertIsInstance(i, Room)
         else:
-            # The list should be empty
-            self.assertEqual(len(self.amity.rooms), 0)
+            self.assertIsInstance(self.amity.rooms, list)
+
 
     def test_people_attr_is_list(self):
+        print("FAiling please..")
         self.assertIsInstance(self.amity.people, list)
 
     def test_people_attr_is_list_of_person_objs(self):
-        for i in self.amity.people:
-            self.assertIsInstance(i, Person)
+        if self.amity.people:
+            # To Avoid NoneType Error
+            for i in self.amity.rooms:
+                self.assertIsInstance(i, Person)
         else:
-            # The list should be empty
-            self.assertEqual(len(self.amity.people), 0)
+            self.assertIsInstance(self.amity.people, list)
+
+    def test_create_room_returns_list_of_rooms(self):
+        # Should return a list of Rooms
+        rooms = self.amity.create_room(["hogwarts", "camelot"])
+        self.assertIsInstance(rooms, list)
+        self.assertNotEqual(len(rooms), 0)
+        # And if it's not empty. Confirm that the items are Rooms
+        if rooms:
+            for i in rooms:
+                self.assertIsInstance(i, Room) 
+
+    def test_acreate_room_raises_value_error_for_non_string(self):
+        with self.assertRaises(ValueError):
+            self.amity.create_room([66.4, 34])
 
 
 if __name__ == '__main__':
