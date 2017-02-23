4ac1dd3a9d73a7e543b124a23c9057f22598fc9b
andela-wcyn <cynthia.abura@andela.com>
2017-02-23 13:55:45 +0300

    [Chore] Add amity test for people attribute

--- tests/test_amity.py
+++ tests/test_amity.py
@@ -1,18 +1,26 @@
 import unittest
 
+
 class TestAmity(unittest.TestCase):
+    amity = Amity()
 
     def test_rooms_attr_is_list(self):
-        amity = Amity()
         self.assertIsInstance(amity.rooms, list)
 
     def test_rooms_attr_is_list_of_room_objs(self):
-        amity = Amity()
         for i in amity.rooms:
             self.assertIsInstance(i, Room)
         else:
             # The list should be empty
             self.assertEqual(len(amity.rooms), 0)
 
-    
+    def test_people_attr_is_list(self):
+        self.assertIsInstance(amity.people, list)
+
+    def test_people_attr_is_list_of_person_objs(self):
+        for i in amity.people:
+            self.assertIsInstance(i, Person)
+        else:
+            # The list should be empty
+            self.assertEqual(len(amity.people), 0)
 
